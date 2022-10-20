fcfcf8de84142f1c1c900963c6071cfb8c62969f
MountainDragon <dr.katmorales@gmail.com>
2022-10-20 16:47:30 -0600

    Add a working function for one game round

--- rockPaperScissors/javascript.js
+++ rockPaperScissors/javascript.js
@@ -8,6 +8,7 @@ function getComputerChoice(){
  let computerSelection = getComputerChoice();
  let playerSelection;
 
+ while(playerSelection == undefined){
  let answer = prompt("Your move! Rock, paper or scissors?", "Type here:");
 answer = answer[0].toUpperCase().concat(answer.substring(1).toLowerCase());
  
@@ -17,6 +18,47 @@ answer = answer[0].toUpperCase().concat(answer.substring(1).toLowerCase());
 }else{
      playerSelection = answer;
 }
+}
+let playerScore = 0;
+let computerScore = 0;
+
+let playRound = (computerSelection, playerSelection) => {
+
+switch (true){
+    case computerSelection===playerSelection:
+        console.log("It's a tie!");
+        alert("It's a tie!");
+        break;
+    
+    case computerSelection === 'Rock' && playerSelection ==='Scissors':
+        console.log("You Lose! Rock beats Scissors");
+        alert("You Lose! Rock beats Scissors");
+        computerScore+=1;
+        break;
+    
+    case computerSelection === 'Paper' && playerSelection ==='Rock':
+        console.log("You Lose! Paper beats Rock");
+        alert("You Lose! Paper beats Rock");
+        computerScore+=1;
+        break;
+
+    case computerSelection === 'Scissors' && playerSelection ==='Paper':
+        console.log("You Lose! Scissors beats Paper");
+        alert("You Lose! Scissors beats Paper");
+        computerScore+=1;
+        break;
+
+    default:
+        console.log("You WON this round!");
+        alert("You WON this round!");
+        playerScore+=1;
+
+}
+
+}
+
+console.log(playRound(computerSelection, playerSelection));
+
 
 
 
