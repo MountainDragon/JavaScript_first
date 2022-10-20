8177e01cbfb2636e294e61f2527b5a9a388a2814
MountainDragon <dr.katmorales@gmail.com>
2022-10-20 16:08:39 -0600

    Add code, that gets answer from the user and validates it

--- rockPaperScissors/javascript.js
+++ rockPaperScissors/javascript.js
@@ -5,4 +5,24 @@ function getComputerChoice(){
    return randomResult;
 }
  //will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+ let computerSelection = getComputerChoice();
+ let playerSelection;
+
+ let answer = prompt("Your move! Rock, paper or scissors?", "Type here:");
+answer = answer[0].toUpperCase().concat(answer.substring(1).toLowerCase());
+ 
+    if(choiceArray.indexOf(answer) < 0){
+    alert("Hey! Only rock, paper or scissors, please!");
+  
+}else{
+     playerSelection = answer;
+}
+
+
+
+
+
+
+
+
 
